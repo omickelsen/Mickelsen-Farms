21dd1f9bedf69b70264dc4c10534fb9d91b6a64a
trome23 <romennoodles23@yahoo.com>
2019-07-02 23:30:08 -0600

    backend set up to have users log in with email and password, with authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken')
-const config = require('config')
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User')
