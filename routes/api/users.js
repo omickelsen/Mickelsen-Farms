c5fb6ac4a48b40aefac0511ecb3a1d95bbb6dff4
trome23 <romennoodles23@yahoo.com>
2019-07-02 22:54:14 -0600

    can now post new users to db that have jsonwebtokens attached to them

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User')
@@ -56,15 +58,26 @@ router.post(
             user.password = await bcrypt.hash(password, salt);
 
             await user.save();
-
             // Return JSONwebtoken
-            
-            res.send('User registered');
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload, 
+                config.get('jwtSecret'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if(err) throw err;
+                    res.json({ token });
+                }
+            ); 
         } catch(err) {
             console.log(err.message);
             res.status(500).send('Server error')            
         }
-
     }
 );
 
