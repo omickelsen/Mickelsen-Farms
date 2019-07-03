5e0227b6bdc8febccfbbe999c08bdc278d711ed3
trome23 <romennoodles23@yahoo.com>
2019-07-02 21:49:16 -0600

    express validator added and user model created

--- server.js
+++ server.js
@@ -6,8 +6,17 @@ const app = express();
 //Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }))
+
 app.get('/', (req, res) => res.send('API Running'));
 
-const PORT = process.env.PORT || 5000
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
