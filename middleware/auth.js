f95b841182f2e7846fe6c1077aafd5632b8fe4e3
trome23 <romennoodles23@yahoo.com>
2019-07-03 21:54:24 -0600

    post can be deleted from db on the backend using postman

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,7 @@ module.exports = function(req, res, next) {
 
     // Check if not token
     if(!token) {
-        return res.status(401).json({ msg: 'No token, ahtorization denied' })
+        return res.status(401).json({ msg: 'No token, authorization denied' })
     }
 
     // Verify token
