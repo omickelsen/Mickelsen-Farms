5687fdb69855bc7c0744640d507baa193f2ede0b
trome23 <romennoodles23@yahoo.com>
2019-07-05 20:49:34 -0600

    can register user, and stores them in db

--- client/src/Components/auth/Register.jsx
+++ client/src/Components/auth/Register.jsx
@@ -1,4 +1,5 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState } from 'react';
+import axios from 'axios';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -12,13 +13,34 @@ const Register = () => {
 
   const onChange = e => setFormData ({ ...formData, [e.target.name]: e.target.value});
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if(password !== password2) {
       console.log('Passwords do not match');
     } else {
-      console.log(formData);
+        console.log('Success');
       
+      // const newUser = {
+      //   name,
+      //   email,
+      //   password
+      // }
+      // try {
+      //   const config = {
+      //     headers: {
+      //       'Content-Type': 'Application/json'
+      //     }
+      //   }
+
+      //   const body = JSON.stringify(newUser);
+
+      //   const res = await axios.post('/api/users', body, config);
+      //     console.log(res.data);
+        
+      // } catch (err) {
+      //     console.log(err.response.data);
+          
+      // }
     }
   }
 
