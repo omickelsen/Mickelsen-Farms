ac8496f5974cf6e1cb654ecc38a51fb3e87bb507
trome23 <romennoodles23@yahoo.com>
2019-07-05 20:21:24 -0600

    buttons at top of page link to login/register and those basic pages are setup

--- client/src/Components/auth/Login.jsx
+++ client/src/Components/auth/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Login = () => {
   return(
-    <div>
+    <div className='login' id='login'>
         Log in
     </div>
   )
--- client/src/Components/auth/Register.jsx
+++ client/src/Components/auth/Register.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = e => setFormData ({ ...formData, [e.target.name]: e.target.value});
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if(password !== password2) {
+      console.log('Passwords do not match');
+    } else {
+      console.log(formData);
+      
+    }
+  }
+
   return(
-    <div>
-      Register
-    </div>
+    <Fragment>
+      <div className='register' id='register'>
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h4 className="display-4 text-center">Sign Up</h4>
+              <p className="lead text-center">Create your User account</p>
+              <form className='form' onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                  <input type="text" className="form-control form-control-lg" placeholder="Name" name="name" value={name} onChange={e=> onChange(e)} required />
+                </div>
+                <div className="form-group">
+                  <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email" value={email} onChange={e=> onChange(e)} required />
+                </div>
+                <div className="form-group">
+                  <input type="password" className="form-control form-control-lg" placeholder="Password" name="password" value={password} onChange={e=> onChange(e)} required/>
+                </div>
+                <div className="form-group">
+                  <input type="password" className="form-control form-control-lg" placeholder="Confirm Password" name="password2" value={password2} onChange={e=> onChange(e)} required/>
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-2" />
+              </form>
+            </div>
+          </div>
+      </div>
+    </Fragment>
   )
 }
 
