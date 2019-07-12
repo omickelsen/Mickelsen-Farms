040132ceb9de0170e5b99eda383da20a0872efb1
Michael Olsen <michaelolsen@Michaels-MacBook-Air.local>
2019-07-11 22:01:54 -0600

    working-carousel

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,31 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Hero from '../components/Hero';
 import Header from '../components/Header';
 import About from '../components/About';
-import Parallax from '../components/Parallax';
+import MainPageCarousel from '../components/MainPageCarousel';
 import Services from '../components/Services';
 import Team from '../components/Team';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
 import ScrollTop from '../components/ScrollTop';
 import MainCalendar from '../components/MainCalendar';
-import '../styles/Bootstrap.min.css';
-import '../styles/App.css';
+import '../Styles/Bootstrap.min.css';
+import '../Styles/App.css';
 
 const Home = () => {
-    return(
-    <div>
-        <Hero/>
-        <Header />
-        <About />
-        <Parallax />
-        <Services />
-        <MainCalendar />
-        <Team />
-        <Contact />
-        <Footer />
-        <ScrollTop /> 
-    </div> 
+    return (
+        <div>
+            <Hero />
+            <Header />
+            <About />
+            <MainPageCarousel />
+            <Services />
+            <MainCalendar />
+            <Team />
+            <Contact />
+            <Footer />
+            <ScrollTop />
+        </div>
     )
 }
 
