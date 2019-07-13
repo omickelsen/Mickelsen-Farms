b025dadc21c49d572f9b246afb78b45062b31728
Michael Olsen <michaelolsen@Michaels-MacBook-Air.local>
2019-07-12 22:20:57 -0600

    styling service cards and carousel

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const connectDB = require('./config/db')
+const express = require( 'express' );
+const connectDB = require( './config/db' )
 
 const app = express();
 
@@ -7,16 +7,16 @@ const app = express();
 connectDB();
 
 // Init Middleware
-app.use(express.json({ extended: false }))
+app.use( express.json( { extended: false } ) )
 
-app.get('/', (req, res) => res.send('API Running'));
+app.get( '/', ( req, res ) => res.send( 'API Running' ) );
 
 // Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use( '/api/users', require( './routes/api/users' ) );
+app.use( '/api/auth', require( './routes/api/auth' ) );
+app.use( '/api/profile', require( './routes/api/profile' ) );
+app.use( '/api/posts', require( './routes/api/posts' ) );
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen( PORT, () => console.log( `Server started on port ${PORT}` ) );
