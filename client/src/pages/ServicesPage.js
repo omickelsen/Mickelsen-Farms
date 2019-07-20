e9d904e41ebbe67fa5191ba0087e18e6f0865328
Michael Olsen <michaelolsen@michaels-air.uconnect.utah.edu>
2019-07-20 12:55:13 -0600

    footers and headers on pages

--- client/src/pages/ServicesPage.js
+++ client/src/pages/ServicesPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Header from '../components/layout/Header'
+import Header from '../components/layout/Header';
 import { Link } from 'react-router-dom';
-import Services from '../components/Services'
-import Footer from '../components/layout/Footer'
+import Services from '../components/Services';
+import Footer from '../components/layout/Footer';
 
 
 
