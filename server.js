994a5cac15d62e92d57926628978c0682a926970
omickelsen <omickelsen@gmail.com>
2019-07-22 21:41:43 -0600

    got rid of api running

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ connectDB();
 // Init Middleware
 app.use( express.json( { extended: false } ) )
 
-app.get( '/', ( req, res ) => res.send( 'API Running' ) );
+// app.get( '/', ( req, res ) => res.send( 'API Running' ) );
 
 // Define Routes
 app.use( '/api/users', require( './routes/api/users' ) );
