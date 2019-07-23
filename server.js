41afc2ae4c0c2b43941a9c6db53cecd6a1bff60f
omickelsen <omickelsen@gmail.com>
2019-07-22 21:50:17 -0600

    adding express build

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ connectDB();
 app.use( express.json( { extended: false } ) )
 
 // app.get( '/', ( req, res ) => res.send( 'API Running' ) );
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 // Define Routes
 app.use( '/api/users', require( './routes/api/users' ) );
