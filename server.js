ec16049968bdbd8dfc55d2b8509930c570630d57
omickelsen <omickelsen@gmail.com>
2019-07-22 21:51:30 -0600

    fixing server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require( 'express' );
 const connectDB = require( './config/db' )
 
 const app = express();
-
+const path = require('path');
 //Connect Database
 connectDB();
 
@@ -15,7 +15,6 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
   
     // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
