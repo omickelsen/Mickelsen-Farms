e11d9daa6b296dcf2c61ffad6973630a8d2656ba
Jeff Elsey <jcelsey@gmail.com>
2019-12-19 21:55:14 -0700

    Moved fall back route after all the other routes

--- server.js
+++ server.js
@@ -9,23 +9,23 @@ connectDB();
 // Init Middleware
 app.use( express.json( { extended: false } ) )
 
-// app.get( '/', ( req, res ) => res.send( 'API Running' ) );
-if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('client/build'));
-  
-    // Express serve up index.html file if it doesn't recognize route
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
-
 // Define Routes
 app.use( '/api/users', require( './routes/api/users' ) );
 app.use( '/api/auth', require( './routes/api/auth' ) );
 app.use( '/api/profile', require( './routes/api/profile' ) );
 app.use( '/api/posts', require( './routes/api/posts' ) );
 app.use( '/api/event', require( './routes/api/event' ) );
+
+// app.get( '/', ( req, res ) => res.send( 'API Running' ) );
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 const PORT = process.env.PORT || 5000;
 
