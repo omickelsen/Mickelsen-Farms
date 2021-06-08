ec93435ee87aca84babe55a1267ef7ae00b792c8
Orie Mickelsen <omickelsen@gmail.com>
2021-06-08 11:36:28 -0600

    removed Izzy and Olivia pages

--- client/src/pages/Izzy-Barnes.js
+++ client/src/pages/Izzy-Barnes.js
@@ -1,75 +1,75 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Header from '../components/layout/Header'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Izzy from '../images/izzy.jpg'
-import Accordion from 'react-bootstrap/Accordion'
-import Card from 'react-bootstrap/Card'
-import Footer from '../components/layout/Footer'
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import Header from '../components/layout/Header'
+// import Container from 'react-bootstrap/Container'
+// import Row from 'react-bootstrap/Row'
+// import Col from 'react-bootstrap/Col'
+// import Izzy from '../images/izzy.jpg'
+// import Accordion from 'react-bootstrap/Accordion'
+// import Card from 'react-bootstrap/Card'
+// import Footer from '../components/layout/Footer'
 
 
 
 
 
 
-const IzzyBarnes = () => {
-    return (
+// const IzzyBarnes = () => {
+//     return (
 
-        <div>
-            <Header />
-            <div className="container">
-                <div className='service-container'>
-                    <h1 className="service-title">Izzy Barnes</h1>
-                </div>
-            </div>
-            <Container>
-                <Row>
-                    <Col > <div className="row">
-                        <div className="photo-container">
-                            <img src={Izzy} className="familyphoto" />
-                        </div>
-                    </div>
-                    </Col>
-                    <Col>
-                        {/* <div className="second-container"> */}
-                        <div>
-                            <div className="row">
-                                <div className="about-farm-container">
-                                    <h1 className="about-farm-title">About Izzy</h1>
-                                    <p>
-                                        <ul className="about-farm-list">
-                                            <li>Izzy is so excited to share her love and passion for horses with her community. She will be graduating from Lone Peak High School this year receiving her associates's certificate in Child Development. Izzy has loved the many experiences that horses have brought to her life. She doesn't remember a moment when she wasn't completely in love with horses! She has been teaching horseback riding since she was 12 years old. Izzy has loved spending the last few years being part of her equine 4-H club as Vice President, President and now Leader! She broke and trained her first horse when she was 13 and has loved training horses ever since. When Izzy is not with horses, she loves hiking with her dogs, running, camping and spending countless hours as a nanny to her "kiddos". She was the Junior Rodeo Queen for Lindon last year, and this year will represent Pleasant Grove as the 2019-20 Strawberry Days Rodeo 1st attendant. Izzy can't wait to teach lessons as much as possible because it combines her passions in life --horses and children. Izzy is also proficient to give lessons on any of the horses at Mickelsen Family Farms and very familiar with them.</li>
-                                        </ul>
-                                    </p>
-                                </div>
-                                {/* </div> */}
-                            </div>
-                        </div>
-                        <div>
-                            <div className="row">
-                                <div className="team-container">
-                                    <h1 className="about-team-title">Contact Me</h1>
-                                    <h1>
-                                        <nav className="nav social-nav pull-center d-none d-lg-inline">
-                                            <a href="https://www.facebook.com/izzy.barnes.90"><i className="fab fa-facebook"></i></a> <i className="fa fa-envelope"> Belle.barnes20@gmail.com</i>
-                                            <i className="fa fa-phone"></i>
-                                            <p>Izzy Barnes: +1 385-225-0712</p>
-                                        </nav>
-                                    </h1>
-                                </div>
-                            </div>
-                        </div>
+//         <div>
+//             <Header />
+//             <div className="container">
+//                 <div className='service-container'>
+//                     <h1 className="service-title">Izzy Barnes</h1>
+//                 </div>
+//             </div>
+//             <Container>
+//                 <Row>
+//                     <Col > <div className="row">
+//                         <div className="photo-container">
+//                             <img src={Izzy} className="familyphoto" />
+//                         </div>
+//                     </div>
+//                     </Col>
+//                     <Col>
+//                         {/* <div className="second-container"> */}
+//                         <div>
+//                             <div className="row">
+//                                 <div className="about-farm-container">
+//                                     <h1 className="about-farm-title">About Izzy</h1>
+//                                     <p>
+//                                         <ul className="about-farm-list">
+//                                             <li>Izzy is so excited to share her love and passion for horses with her community. She will be graduating from Lone Peak High School this year receiving her associates's certificate in Child Development. Izzy has loved the many experiences that horses have brought to her life. She doesn't remember a moment when she wasn't completely in love with horses! She has been teaching horseback riding since she was 12 years old. Izzy has loved spending the last few years being part of her equine 4-H club as Vice President, President and now Leader! She broke and trained her first horse when she was 13 and has loved training horses ever since. When Izzy is not with horses, she loves hiking with her dogs, running, camping and spending countless hours as a nanny to her "kiddos". She was the Junior Rodeo Queen for Lindon last year, and this year will represent Pleasant Grove as the 2019-20 Strawberry Days Rodeo 1st attendant. Izzy can't wait to teach lessons as much as possible because it combines her passions in life --horses and children. Izzy is also proficient to give lessons on any of the horses at Mickelsen Family Farms and very familiar with them.</li>
+//                                         </ul>
+//                                     </p>
+//                                 </div>
+//                                 {/* </div> */}
+//                             </div>
+//                         </div>
+//                         <div>
+//                             <div className="row">
+//                                 <div className="team-container">
+//                                     <h1 className="about-team-title">Contact Me</h1>
+//                                     <h1>
+//                                         <nav className="nav social-nav pull-center d-none d-lg-inline">
+//                                             <a href="https://www.facebook.com/izzy.barnes.90"><i className="fab fa-facebook"></i></a> <i className="fa fa-envelope"> Belle.barnes20@gmail.com</i>
+//                                             <i className="fa fa-phone"></i>
+//                                             <p>Izzy Barnes: +1 385-225-0712</p>
+//                                         </nav>
+//                                     </h1>
+//                                 </div>
+//                             </div>
+//                         </div>
 
-                    </Col>
-                </Row>
-            </Container>
-            <Footer />
-        </div>
-    )
-}
+//                     </Col>
+//                 </Row>
+//             </Container>
+//             <Footer />
+//         </div>
+//     )
+// }
 
 
 
-export default IzzyBarnes
+// export default IzzyBarnes
